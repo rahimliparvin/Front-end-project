0af39155af40aa4b5e2d9c02b2ab3f0bf63208f1
rahimliparvin <parvinvr@code.edu.az>
2023-03-03 07:01:00 -0800

    add tab menu write

--- sophia-ecommerce/assets/js/home.js
+++ sophia-ecommerce/assets/js/home.js
@@ -153,6 +153,30 @@ $(document).ready(function () {
 	})
 
 
+	const tabLink = document.querySelectorAll(".tab-menu-link");
+const tabContent = document.querySelectorAll(".tab-bar-content");
+
+tabLink.forEach((item) => {
+  item.addEventListener("click", activeTab);
+});
+
+function activeTab(item) {
+  const btnTarget = item.currentTarget;
+  const content = btnTarget.dataset.content;
+
+  tabContent.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  tabLink.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  document.querySelector("#" + content).classList.add("is-active");
+  btnTarget.classList.add("is-active");
+}
+
+
 
 	// let cardsicons = document.querySelectorAll("#carouselcards .swiper-slide");
 
